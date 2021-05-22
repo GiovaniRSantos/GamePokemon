659a267b0882215c948e95a17563fc20da28e62b
kali <kali@kali.kali>
2021-05-22 20:43:22 -0300

    inicio do projeto pokemon

---
+++ pokemon.py
@@ -0,0 +1,12 @@
+class Pokemon:
+    def __init__(self, tipo, especie):
+        self.tipo = tipo
+        self.especie = especie
+
+    def __str__(self):
+        return "{} ({})".format(self.especie,self.tipo)
+
+    def atacar(self,pokemon):
+        print("{} atacou {}!".format(self.especie, pokemon.especie))
+
+
