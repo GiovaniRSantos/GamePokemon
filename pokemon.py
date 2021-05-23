51891645325cb29d360db016aff42a12f40f64da
kali <kali@kali.kali>
2021-05-22 21:05:48 -0300

    heranca das class

--- pokemon.py
+++ pokemon.py
@@ -1,12 +1,16 @@
 class Pokemon:
-    def __init__(self, tipo, especie):
+    def __init__(self, tipo, especie,level=1, nome=None):
         self.tipo = tipo
         self.especie = especie
+        self.level = level
+        self.nome = nome
 
     def __str__(self):
-        return "{} ({})".format(self.especie,self.tipo)
+        return "{} ({})".format(self.nome,self.level)
 
     def atacar(self,pokemon):
         print("{} atacou {}!".format(self.especie, pokemon.especie))
 
-
+class PokemonEletrico(Pokemon):
+    def atacar(self,pokemon):
+        print("{} lancou um raio do trovao em {}".format(self,pokemon))
